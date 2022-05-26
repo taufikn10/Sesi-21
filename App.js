c7a36518afb72e0ccfef9c31e528a769d3e9f3e1
Taufik Nurrahman <official.taufik980@gmail.com>
2022-05-26 20:05:48 +0700

    Sesi 21

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./components/Header";
+import Posts from "./components/Posts";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Posts />
+    </>
+  );
+}
